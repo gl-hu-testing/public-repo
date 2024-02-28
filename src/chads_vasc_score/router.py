cc4816f4e22bcbc9ecc2642dae0b9a741eb5b696
gl-hu-testing <153951502+gl-hu-testing@users.noreply.github.com>
2024-02-28 01:25:48 -0500

    Update router.py

--- src/chads_vasc_score/router.py
+++ src/chads_vasc_score/router.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, Depends
 
 from src.chads_vasc_score.schemas import ChadsVascScore
 from src.chads_vasc_score.utils import unpack_and_calc_cvs
-
+from src.config import settings, Settings
 router = APIRouter()
 
 
@@ -24,3 +24,18 @@ async def index(score: int = Depends(unpack_and_calc_cvs)) -> ChadsVascScore:
         CHA₂DS₂-VASc score.
     """
     return ChadsVascScore(score=score)
+
+
+@router.post(
+    "/settings",
+    status_code=200,
+    name="Settings dump",
+    description="test settings dump.",
+)
+async def test_settings() -> Settings:
+    """Test dump settings.
+
+    Returns:
+        Settingso bject.
+    """
+    return settings.dict()
